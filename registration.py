beadaf3c3b94b6cf0449f409a95950b6e2083241
sachin <sachinraj1652@email.com>
2024-08-29 17:25:10 +0530

    create member frame

--- registration.py
+++ registration.py
@@ -17,6 +17,9 @@ class Registration:
         title = Label(self.root,text="Member Registration form", font=("monotype corsive",30,"bold"),bd=5,relief=GROOVE,bg="#E6005C",fg="#000000")
         title.pack(side=TOP,fill=X)
 
+        manage_Frame = Frame(self.root,bd=4,relief=RIDGE,bg="#001a66")
+        manage_Frame.place(x=20,y=100,width=450,height=630)
+
 
 if __name__ == "__main__":
     root = Tk()
