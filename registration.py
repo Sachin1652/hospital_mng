f931ee0ec5b29381d22092cbd27b1b8fba80d5db
sachin <sachinraj1652@email.com>
2024-08-29 21:51:56 +0530

    member detials complete

--- registration.py
+++ registration.py
@@ -60,6 +60,44 @@ class Registration:
         member_reftxt = Entry(manage_Frame,font=("arial",15,"bold"),state=DISABLED,Text=Ref)
         member_reftxt.grid(row=2,column=1,pady=5,padx=10,sticky="w")
 
+        member_fnamelbl = Label(manage_Frame,text="First Name",font=("arial",15,"bold"),bg="#001a66",fg="white")
+        member_fnamelbl.grid(row=3,column=0,pady=10,sticky="w")
+
+        member_fnametxt = Entry(manage_Frame,font=("arial",15,"bold"),textvariable=Firstname)
+        member_fnametxt.grid(row=3,column=1,pady=5,padx=10,sticky="w")
+
+        member_lnamelbl = Label(manage_Frame,text="Last Name",font=("arial",15,"bold"),bg="#001a66",fg="white")
+        member_lnamelbl.grid(row=4,column=0,pady=10,sticky="w")
+
+        member_lnametxt = Entry(manage_Frame,font=("arial",15,"bold"),textvariable=Lastname)
+        member_lnametxt.grid(row=4,column=1,pady=5,padx=10,sticky="w")
+
+        member_mobilelbl = Label(manage_Frame,text="Mobile No",font=("arial",15,"bold"),bg="#001a66",fg="white")
+        member_mobilelbl.grid(row=5,column=0,pady=10,sticky="w")
+
+        member_mobiletxt = Entry(manage_Frame,font=("arial",15,"bold"),textvariable=Mobile_no)
+        member_mobiletxt.grid(row=5,column=1,pady=5,padx=10,sticky="w")
+
+        member_addresslbl = Label(manage_Frame,text="Address",font=("arial",15,"bold"),bg="#001a66",fg="white")
+        member_addresslbl.grid(row=6,column=0,pady=10,sticky="w")
+
+        member_addresstxt = Entry(manage_Frame,font=("arial",15,"bold"),textvariable=Address)
+        member_addresstxt.grid(row=6,column=1,pady=5,padx=10,sticky="w")
+
+        member_pincodelbl = Label(manage_Frame,text="Pin Code",font=("arial",15,"bold"),bg="#001a66",fg="white")
+        member_pincodelbl.grid(row=7,column=0,pady=10,sticky="w")
+
+        member_pincodetxt = Entry(manage_Frame,font=("arial",15,"bold"),textvariable=Pincode)
+        member_pincodetxt.grid(row=7,column=1,pady=5,padx=10,sticky="w")
+
+        member_genderlbl = Label(manage_Frame,text="Gender",font=("arial",15,"bold"),bg="#001a66",fg="white")
+        member_genderlbl.grid(row=8,column=0,pady=10,sticky="w")
+
+        member_gendercmb = ttk.Combobox(manage_Frame,Text=var4,state="readonly",font=("arial",15,"bold"),width=19)
+        member_gendercmb['values']=("","Male","Female","Other")
+        member_gendercmb.current(0) #when nothing it will set auto empty 
+        member_gendercmb.grid(row=8,column=1,pady=5,padx=10,sticky="w")
+
 
 if __name__ == "__main__":
     root = Tk()
